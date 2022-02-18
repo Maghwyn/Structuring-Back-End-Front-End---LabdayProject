6205bc3ccf4c527014832ebec9fee5f7792db32a
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-02-18 19:44:43 +0100

    Connected the BDD and routes templates.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { getUserById, createUser, updateUser, deleteUser } from "./controllers/usersCRUD.js";
 
+const PORT = 5000;
 const app = express();
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = "";
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users/:id', getUserById);
+app.post('/users', createUser)
+app.put('/users/:id', updateUser)
+app.delete('/users/:id', deleteUser)
+  
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}.`)
+})
